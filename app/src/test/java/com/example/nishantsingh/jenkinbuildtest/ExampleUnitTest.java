2db8588ae59462500475364df6ff16dde11c8941
NishantSingh135 <nishant.singh@infostretch.com>
2017-06-19 11:26:45 +0530

    error ramoved

--- app/src/test/java/com/example/nishantsingh/jenkinbuildtest/ExampleUnitTest.java
+++ app/src/test/java/com/example/nishantsingh/jenkinbuildtest/ExampleUnitTest.java
@@ -18,7 +18,7 @@ public class ExampleUnitTest {
 
     @Test
     public void Substraction_isCorrect() throws Exception {
-        assertEquals(4, 8 - 4);
+        assertEquals(3, 8 - 5);
     }
 
 }
